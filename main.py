56a0d60e0a9a235f07346dc907e5f676098400df
Lucio <djelectrogamer.verynexus@gmail.com>
2021-11-24 15:01:30 +0100

    test

--- main.py
+++ main.py
@@ -3,75 +3,93 @@ import pygame
 from pygame.constants import FULLSCREEN, RESIZABLE
 from pygame.locals import *
 import pygame.freetype
+from datetime import datetime, timedelta
 
 #Import les sous programmes
 import menu
-
-#Réglage de la résolution de la fenêtre
-pix_lar = 1280
-pix_long = 720
-
-#Définitions de certaines variables
-run = True
-clock = pygame.time.Clock()
+import j1
 
 #Initialisation de la fenêtre graphique
 pygame.init()
-screen = pygame.display.set_mode((pix_lar, pix_long))
-screen.fill((80 ,80 , 80))
 pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+def init_screen(pix_lar, pix_long):
+	screen = pygame.display.set_mode((pix_lar, pix_long))
+	screen.fill((80 ,80 , 80))
+	return(screen)
 
 #Définition de la police d'écriture
-GAME_FONT_menu = pygame.freetype.Font('assets/BD_Cartoon_Shout.ttf', 16)
-GAME_FONT_stats = pygame.freetype.Font('assets/Spongy.otf', 16)
+def font_menu():
+	GAME_FONT_menu = pygame.freetype.Font('assets/BD_Cartoon_Shout.ttf', 16)
+	return(GAME_FONT_menu)
+
+def font_stats():
+	GAME_FONT_stats = pygame.freetype.Font('assets/Spongy.otf', 16)
+	return(GAME_FONT_stats)
+
+GAME_FONT_menu = font_menu()
+GAME_FONT_stats = font_stats()
 
 #Définition du titre de la fenêtre
 pygame.display.set_caption("Super Bomb")
 
-#Boucle néccéssaire pour le bon fonctionnement de la partie graphique
-while run:
-	for event in pygame.event.get():
-		if event.type == KEYDOWN:
-			if event.key == K_ESCAPE:
+def boucle_main():
+	#Réglage de la résolution de la fenêtre
+	pix_lar = 1280
+	pix_long = 720
+
+	screen = init_screen(pix_lar, pix_long)
+
+	#Définitions de certaines variables
+	run = True
+	clock = pygame.time.Clock()
+
+	#Boucle néccéssaire pour le bon fonctionnement de la partie graphique
+	while run:
+		for event in pygame.event.get():
+			if event.type == KEYDOWN:
+				if event.key == K_ESCAPE:
+					run = False
+			
+			if event.type == QUIT:
 				run = False
 		
-		if event.type == QUIT:
-			run = False
-	
-	#Détection click souris pour le menu
-	mousePos = pygame.mouse.get_pos()
-	if mousePos[0] > 533 and mousePos[0] < 708 and mousePos[1] > 644 and mousePos[1] < 679 :
-		#pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
-		if event.type == MOUSEBUTTONDOWN:
-			run = False
+		#Détection click souris pour le menu
+		mousePos = pygame.mouse.get_pos()
+		if mousePos[0] > 533 and mousePos[0] < 708 and mousePos[1] > 644 and mousePos[1] < 679 :
+			#pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+			if event.type == MOUSEBUTTONDOWN:
+				run = False
+			
+		'''if mousePos[0] > 500 and mousePos[0] < 757 and mousePos[1] > 177 and mousePos[1] < 220 : 
+			pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+			if event.type == MOUSEBUTTONDOWN:
+				print("Mode Solo")
+		'''
 		
-	'''if mousePos[0] > 500 and mousePos[0] < 757 and mousePos[1] > 177 and mousePos[1] < 220 : 
-		pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
-		if event.type == MOUSEBUTTONDOWN:
-			print("Mode Solo")
-	'''
-	
-	if mousePos[0] > 476 and mousePos[0] < 774 and mousePos[1] > 249 and mousePos[1] < 288 : 
-		#pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
-		if event.type == MOUSEBUTTONDOWN:
-			print("Mode multijoueur")
+		if mousePos[0] > 476 and mousePos[0] < 774 and mousePos[1] > 249 and mousePos[1] < 288 : 
+			#pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+			if event.type == MOUSEBUTTONDOWN:
+				pygame.quit()
+				exec(open("j1.py").read())
 
-	if not mousePos[0] > 533 and mousePos[0] < 708 and mousePos[1] > 644 and mousePos[1] < 679 :
-		pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
-	
-	if not mousePos[0] > 476 and mousePos[0] < 774 and mousePos[1] > 249 and mousePos[1] < 288 :
-		pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+		if not mousePos[0] > 533 and mousePos[0] < 708 and mousePos[1] > 644 and mousePos[1] < 679 :
+			pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+		
+		if not mousePos[0] > 476 and mousePos[0] < 774 and mousePos[1] > 249 and mousePos[1] < 288 :
+			pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
 
-	#Récupère les infos de la fenêtre (La résolution)
-	ecran = pygame.display.Info()
+		#Récupère les infos de la fenêtre (La résolution)
+		ecran = pygame.display.Info()
 
-	#Affichage du menu
-	menu.menu_titre(screen, GAME_FONT_menu, ecran.current_w)
-	menu.menu_solo(screen, GAME_FONT_menu, ecran.current_w, ecran.current_h)
-	menu.menu_multi_loc(screen, GAME_FONT_menu, ecran.current_w, ecran.current_h)
-	menu.menu_quit(screen, GAME_FONT_menu, ecran.current_w, ecran.current_h)
-	
+		#Affichage du menu
+		menu.menu_titre(screen, GAME_FONT_menu, ecran.current_w)
+		menu.menu_solo(screen, GAME_FONT_menu, ecran.current_w, ecran.current_h)
+		menu.menu_multi_loc(screen, GAME_FONT_menu, ecran.current_w, ecran.current_h)
+		menu.menu_quit(screen, GAME_FONT_menu, ecran.current_w, ecran.current_h)
+		
+		#Limite de FPS
+		clock.tick(60)
+		pygame.display.flip()
 
-	#Limite de FPS
-	clock.tick(60)
-	pygame.display.flip()
+
+boucle_main()
---
+++ stats.py
@@ -0,0 +1,29 @@
+#Import les librairies nécéssaires
+import pygame
+from pygame import mouse
+from pygame import event
+from pygame.constants import FULLSCREEN, RESIZABLE
+from pygame.locals import *
+import pygame.freetype
+
+import main
+
+pygame.freetype.init()
+
+GAME_FONT_stats = pygame.freetype.Font('assets/Spongy.otf', 16)
+
+def button_quit(screen, lar, long):
+	GAME_FONT_stats.render_to(screen, (lar * 0.45, long - 38), "Quitter", (255,255,255), size = 25)
+
+def click_quit():
+	for event in pygame.event.get():
+		mousePos = pygame.mouse.get_pos()
+		if mousePos[0] > 559 and mousePos[0] < 674 and mousePos[1] > 711 and mousePos[1] < 739:
+			pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+			if event.type == MOUSEBUTTONDOWN:
+				print("sa marche !!")
+				main.boucle_main()
+
+		else:
+			pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+	
