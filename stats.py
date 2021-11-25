c49cfbcc6e6cebf8e4bd5fcb1cc4009d10246974
Lucio <djelectrogamer.verynexus@gmail.com>
2021-11-25 09:37:44 +0100

    essaie de tps de jeu

--- stats.py
+++ stats.py
@@ -5,6 +5,7 @@ from pygame import event
 from pygame.constants import FULLSCREEN, RESIZABLE
 from pygame.locals import *
 import pygame.freetype
+import time
 
 pygame.freetype.init()
 
@@ -12,3 +13,5 @@ GAME_FONT_stats = pygame.freetype.Font('assets/Spongy.otf', 16)
 
 def button_quit(screen, lar, long):
 	GAME_FONT_stats.render_to(screen, (lar * 0.45, long - 38), "Quitter", (255,255,255), size = 25)
+
+
